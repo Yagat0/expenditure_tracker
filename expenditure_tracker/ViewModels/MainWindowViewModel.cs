3c740ab28f981bc8812fa935ba25013c415e4f05
Yagat0 <56794097+Yagat0@users.noreply.github.com>
2025-08-01 16:05:28 +0200

    Add/Edit menu UI

--- expenditure_tracker/ViewModels/MainWindowViewModel.cs
+++ expenditure_tracker/ViewModels/MainWindowViewModel.cs
@@ -2,13 +2,51 @@
 using expenditure_tracker.Models;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace expenditure_tracker.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     public ObservableCollection<Expenditure> ExpendituresDg { get; }
+    
+    private bool _isAdd;
+    private bool _isEdit;
+    
+    [ObservableProperty]
+    private bool _isMenuVisible;
 
+    [RelayCommand]
+    private void MenuAddVisible()
+    {
+        IsMenuVisible = true;
+        _isAdd = true;
+        _isEdit = false;
+    }
+
+    [RelayCommand]
+    private void MenuEditVisible()
+    {
+        IsMenuVisible = true;
+        _isAdd = false;
+        _isEdit = true;
+    }
+
+    [RelayCommand]
+    private void MenuCancel()
+    {
+        IsMenuVisible = false;
+        _isAdd = false;
+        _isEdit = false;
+    }
+
+    [RelayCommand]
+    private void MenuSave()
+    {
+        throw new NotImplementedException();
+    }
+    
     public MainWindowViewModel()
     {
         var expenditures = new List<Expenditure>
